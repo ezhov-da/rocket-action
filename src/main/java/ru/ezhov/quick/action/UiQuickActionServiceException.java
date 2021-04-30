b9c878c91786a9c50f94f2ec7d519aff2b00fb8d
ezhov-da <ezhdenis@yandex.ru>
2021-04-30 06:19:22 +0300

    feat: added update menu

---
+++ src/main/java/ru/ezhov/quick/action/UiQuickActionServiceException.java
@@ -0,0 +1,7 @@
+package ru.ezhov.quick.action;
+
+public class UiQuickActionServiceException extends Exception {
+    public UiQuickActionServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
