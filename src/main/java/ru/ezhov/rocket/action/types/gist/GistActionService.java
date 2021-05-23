066443545f1207c78b8e6ffd9b636ad6ca089654
ezhov-da <ezhdenis@yandex.ru>
2021-05-23 15:54:49 +0300

    fix: fix read property for gist

--- src/main/java/ru/ezhov/rocket/action/types/gist/GistActionService.java
+++ src/main/java/ru/ezhov/rocket/action/types/gist/GistActionService.java
@@ -7,16 +7,26 @@ import ru.ezhov.rocket.action.api.RocketActionSettings;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class GistActionService {
     public static final String TOKEN_PROPERTY = "rocket.action.gist.token";
+    private static final Logger LOGGER = Logger.getLogger(GistActionService.class.getName());
 
     public List<Gist> gists(RocketActionSettings settings) throws GistActionServiceException {
         GitHubClient gitHubClient = new GitHubClient();
         String token = settings.settings().get(GistRocketAction.TOKEN);
-        if (token == null) {
-            token = System.getProperty(TOKEN_PROPERTY);
+        if (token == null || "".equals(token)) {
+            token = System.getProperty(TOKEN_PROPERTY, "");
         }
+
+        String debugToken = "";
+        if (token.length() > 5) {
+            debugToken = token.substring(0, 4);
+        }
+        LOGGER.log(Level.INFO, "method=gists gistToken={0}", debugToken);
+
         gitHubClient.setOAuth2Token(token);
         GistService gistService = new GistService(gitHubClient);
         String username = settings.settings().get("username");
