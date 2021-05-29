b9fecc5cdeaa367ff01fd67d5a57d2db625bcef0
ezhov-da <ezhdenis@yandex.ru>
2021-05-29 16:24:51 +0300

    feat: cache directory path changed

--- src/main/java/ru/ezhov/rocket/action/caching/DiskCache.java
+++ src/main/java/ru/ezhov/rocket/action/caching/DiskCache.java
@@ -2,10 +2,8 @@ package ru.ezhov.rocket.action.caching;
 
 import com.google.common.hash.Hashing;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.X509TrustManager;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,21 +11,24 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Optional;
 
 public class DiskCache implements Cache {
-    private String cacheFolder = System.getProperty("java.io.tmpdir") + File.separator;
+    private File cacheFolder = new File("./cache");
 
     @Override
     public Optional<File> fromCache(URL url) {
+        if (!cacheFolder.exists()) {
+            cacheFolder.mkdirs();
+        }
+
         checkQuietly();
         String sha256hexUrl = Hashing
                 .sha256()
                 .hashString(url.toString(), StandardCharsets.UTF_8)
                 .toString();
-        File file = new File(cacheFolder + "si-" + sha256hexUrl);
+        File file = new File(cacheFolder, sha256hexUrl);
         if (file.exists()) {
             return Optional.of(file);
         } else {
@@ -49,20 +50,15 @@ public class DiskCache implements Cache {
     public static void checkQuietly() {
         try {
             HttpsURLConnection
-                    .setDefaultHostnameVerifier(new HostnameVerifier() {
-                        public boolean verify(String hostname,
-                                              SSLSession session) {
-                            return true;
-                        }
-                    });
+                    .setDefaultHostnameVerifier((hostname, session) -> true);
             SSLContext context = SSLContext.getInstance("TLS");
             context.init(null, new X509TrustManager[]{new X509TrustManager() {
                 public void checkClientTrusted(X509Certificate[] chain,
-                                               String authType) throws CertificateException {
+                                               String authType) {
                 }
 
                 public void checkServerTrusted(X509Certificate[] chain,
-                                               String authType) throws CertificateException {
+                                               String authType) {
                 }
 
                 public X509Certificate[] getAcceptedIssuers() {
