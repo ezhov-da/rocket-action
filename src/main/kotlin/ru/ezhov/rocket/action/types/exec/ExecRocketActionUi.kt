3f3842f328437cb5114502e397e04a381182c6a5
ezhov-da <ezhdenis@yandex.ru>
2021-09-17 19:38:36 +0300

    feat: 0.15.7 add listener to run command

--- src/main/kotlin/ru/ezhov/rocket/action/types/exec/ExecRocketActionUi.kt
+++ src/main/kotlin/ru/ezhov/rocket/action/types/exec/ExecRocketActionUi.kt
@@ -1,5 +1,6 @@
 package ru.ezhov.rocket.action.types.exec
 
+import mu.KotlinLogging
 import ru.ezhov.rocket.action.api.RocketAction
 import ru.ezhov.rocket.action.api.RocketActionConfigurationProperty
 import ru.ezhov.rocket.action.api.RocketActionSettings
@@ -16,8 +17,11 @@ import java.awt.datatransfer.StringSelection
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.io.File
+import javax.swing.Icon
 import javax.swing.JMenuItem
 import javax.swing.filechooser.FileSystemView
+
+private val logger = KotlinLogging.logger { }
 
 class ExecRocketActionUi : AbstractRocketAction() {
 
@@ -28,47 +32,9 @@ class ExecRocketActionUi : AbstractRocketAction() {
                 val label = settings.settings()[LABEL]?.takeIf { it.isNotEmpty() } ?: command
                 val description = settings.settings()[DESCRIPTION]?.takeIf { it.isNotEmpty() } ?: command
                 val iconUrl = settings.settings()[ICON_URL].orEmpty()
+                val menuIcon = icon(iconUrl, command)
 
-                val menuItem = JMenuItem(label)
-                var icon = IconService().load(
-                        iconUrl,
-                        IconRepositoryFactory.repository.by(AppIcon.FIRE)
-                )
-                try {
-                    val file = File(command)
-                    if (file.exists()) {
-                        icon = FileSystemView.getFileSystemView().getSystemIcon(file)
-                    }
-                } catch (ex: Exception) {
-                    ex.printStackTrace()
-                }
-                menuItem.icon = icon
-                menuItem.toolTipText = description
-                menuItem.addMouseListener(object : MouseAdapter() {
-                    override fun mouseReleased(e: MouseEvent) {
-                        if (e.button == MouseEvent.BUTTON3) {
-                            val defaultToolkit = Toolkit.getDefaultToolkit()
-                            val clipboard = defaultToolkit.systemClipboard
-                            clipboard.setContents(StringSelection(command), null)
-                            NotificationFactory.notification.show(NotificationType.INFO, "Команда '$command' скопирована в буфер")
-                        } else if (e.button == MouseEvent.BUTTON1) {
-                            try {
-                                if (workingDir.isEmpty()) {
-                                    Runtime.getRuntime().exec(command)
-                                } else {
-                                    Runtime.getRuntime().exec(
-                                            command,
-                                            null,
-                                            File(workingDir)
-                                    )
-                                }
-                            } catch (ex: Exception) {
-                                ex.printStackTrace()
-                            }
-                        }
-                    }
-                })
-                return object : RocketAction {
+                object : RocketAction {
                     override fun contains(search: String): Boolean =
                             label.contains(search, ignoreCase = true)
                                     .or(command.contains(search, ignoreCase = true))
@@ -77,9 +43,61 @@ class ExecRocketActionUi : AbstractRocketAction() {
                             !(settings.id() == actionSettings.id() &&
                                     settings.settings() == actionSettings.settings())
 
-                    override fun component(): Component = menuItem
+                    override fun component(): Component = JMenuItem(label).apply {
+                        icon = menuIcon
+                        toolTipText = description
+                        addActionListener { executeCommand(workingDir, command) }
+                        addMouseListener(object : MouseAdapter() {
+                            override fun mouseReleased(e: MouseEvent) {
+                                if (e.button == MouseEvent.BUTTON3) {
+                                    copyToClipboard(command)
+                                }
+                            }
+                        })
+                    }
                 }
             }
+
+    private fun icon(iconUrl: String, command: String): Icon {
+        var menuIcon = IconService().load(
+                iconUrl,
+                IconRepositoryFactory.repository.by(AppIcon.FIRE)
+        )
+        try {
+            val file = File(command)
+            if (file.exists()) {
+                menuIcon = FileSystemView.getFileSystemView().getSystemIcon(file)
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+        return menuIcon
+    }
+
+    private fun copyToClipboard(command: String) {
+        val defaultToolkit = Toolkit.getDefaultToolkit()
+        val clipboard = defaultToolkit.systemClipboard
+        clipboard.setContents(StringSelection(command), null)
+        NotificationFactory.notification.show(NotificationType.INFO, "Команда '$command' скопирована в буфер")
+    }
+
+    private fun executeCommand(workingDir: String, command: String) {
+        try {
+            if (workingDir.isEmpty()) {
+                Runtime.getRuntime().exec(command)
+            } else {
+                Runtime.getRuntime().exec(
+                        command,
+                        null,
+                        File(workingDir)
+                )
+            }
+        } catch (ex: Exception) {
+            val text = "Ошибка выполнения команды '$command'"
+            logger.warn(ex) { text }
+            NotificationFactory.notification.show(NotificationType.WARN, text)
+        }
+    }
 
     override fun type(): RocketActionType = RocketActionType { "EXEC" }
 
