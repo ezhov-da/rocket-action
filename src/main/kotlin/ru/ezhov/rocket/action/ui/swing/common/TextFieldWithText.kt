8cc9cbdd8cb94f6e3c88614f309c94edc70c9b17
ezhov-da <ezhdenis@yandex.ru>
2021-09-19 11:57:58 +0300

    feat: 0.15.8 text auto selecting added

--- src/main/kotlin/ru/ezhov/rocket/action/ui/swing/common/TextFieldWithText.kt
+++ src/main/kotlin/ru/ezhov/rocket/action/ui/swing/common/TextFieldWithText.kt
@@ -2,9 +2,26 @@ package ru.ezhov.rocket.action.ui.swing.common
 
 import java.awt.Color
 import java.awt.Graphics
+import java.awt.event.FocusEvent
+import java.awt.event.FocusListener
 import javax.swing.JTextField
 
 class TextFieldWithText(private val placeholder: String) : JTextField() {
+
+    init {
+        addFocusListener(object : FocusListener {
+            override fun focusGained(e: FocusEvent?) {
+                if (text.isNotEmpty()) {
+                    selectionStart = 0
+                    selectionEnd = text.length
+                }
+            }
+
+            override fun focusLost(e: FocusEvent?) = Unit
+
+        })
+    }
+
     override fun paint(g: Graphics) {
         super.paint(g)
         this.text.takeIf { it.isEmpty() }?.let {
