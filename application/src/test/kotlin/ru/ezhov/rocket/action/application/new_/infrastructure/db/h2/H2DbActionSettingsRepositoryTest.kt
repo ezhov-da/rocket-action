1c8c7c7164c65eec2de27d498e72c684b618f4ba
ezhov-da <ezhdenis@yandex.ru>
2022-04-23 18:56:33 +0300

    feat: big changes

--- application/src/test/kotlin/ru/ezhov/rocket/action/application/new_/infrastructure/db/h2/H2DbActionSettingsRepositoryTest.kt
+++ application/src/test/kotlin/ru/ezhov/rocket/action/application/new_/infrastructure/db/h2/H2DbActionSettingsRepositoryTest.kt
@@ -6,6 +6,8 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import ru.ezhov.rocket.action.application.new_.domain.model.ActionIdSampleData
+import ru.ezhov.rocket.action.application.new_.domain.model.ActionSettingName
+import ru.ezhov.rocket.action.application.new_.domain.model.ActionSettingValue
 import ru.ezhov.rocket.action.application.new_.infrastructure.db.LiquibaseDbPreparedService
 
 class H2DbActionSettingsRepositoryTest {
@@ -38,6 +40,29 @@ class H2DbActionSettingsRepositoryTest {
             val actionSettings = repo.settings(id).getOrHandle { throw it }
 
             assertThat(actionSettings).isNotNull.extracting { it?.id }.isEqualTo(id)
+        }
+    }
+
+    @Test
+    fun `should be save successfully`() {
+        with(LiquibaseDbPreparedService.prepareH2Db(tempFolder = tempFolder)) {
+            val repo = H2DbActionSettingsRepository(
+                factory = H2DbKtormDbConnectionFactorySampleData.default(factory = dbCredentialsFactory)
+            )
+            val actionId =
+                ActionIdSampleData.default(uuidAsString = "de2a6ba8-c229-11ec-9d64-0242ac120002")
+            val name = ActionSettingName(value = "name123")
+            val value = ActionSettingValue("value123")
+            val actionSettingsFromRepo = repo.settings(actionId).getOrHandle { throw it }
+            val newActionSettings = actionSettingsFromRepo!!.addOrChangeProperty(
+                name = name,
+                value = value,
+            )
+            repo.save(newActionSettings).getOrHandle { throw it }
+
+            val actionSettingsFromRepoAfterSave = repo.settings(actionId).getOrHandle { throw it }
+
+            assertThat(actionSettingsFromRepoAfterSave!!.value(name)!!.value).isEqualTo(value.value)
         }
     }
 }
