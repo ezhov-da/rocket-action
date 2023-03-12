a0fa2df6d33f6e007d467bf0af75a47c36b9a121
DEzhov <DEzhov@sportmaster.ru>
2023-03-13 01:27:58 +0300

    feat: 0.0.2 ui-utils - добавлена поддержка JTextPane с placeholder

---
+++ ui-utils/src/main/kotlin/ru/ezhov/rocket/action/ui/utils/swing/common/TextPaneWithText.kt
@@ -0,0 +1,34 @@
+package ru.ezhov.rocket.action.ui.utils.swing.common
+
+import java.awt.Color
+import java.awt.Graphics
+import java.awt.event.FocusEvent
+import java.awt.event.FocusListener
+import javax.swing.JTextPane
+
+class TextPaneWithText(private val placeholder: String) : JTextPane() {
+
+    init {
+        addFocusListener(object : FocusListener {
+            override fun focusGained(e: FocusEvent?) {
+                if (text.isNotEmpty()) {
+                    selectionStart = 0
+                    selectionEnd = text.length
+                }
+            }
+
+            override fun focusLost(e: FocusEvent?) = Unit
+
+        })
+    }
+
+    override fun paint(g: Graphics) {
+        super.paint(g)
+        this.text.takeIf { it.isEmpty() }?.let {
+            this.placeholder.takeIf { it.isNotEmpty() }?.let { t ->
+                g.color = Color.gray
+                g.drawString(t, 5, height - 5)
+            }
+        }
+    }
+}
